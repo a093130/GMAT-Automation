c14e813def3f0b1f82ef2e56abdc6d0bd64f5072
Colin Helms <colinhelms@outlook.com>
2019-03-01 16:45:28 -0800

    daily build
    Unsuccessful execution.

--- gmat_batcher.py
+++ gmat_batcher.py
@@ -16,6 +16,8 @@ Created on Wed Feb  6 19:17:02 2019
     
 @Change Log:
     10 Jan 2019, Initial baseline, Integration branch.
+    28 Feb 2019, Working Copy committed to Integration.
+    01 Mar 2019, Multiprocessing Enhancement.
                  
 """
 
@@ -31,18 +33,59 @@ import traceback
 import getpass
 from pathlib import Path
 import subprocess as sp
+import multiprocessing as mp
 from PyQt5.QtWidgets import(QApplication, QFileDialog)
 
 cpto = 300
+rsrv_cpus = 2
+
 """ Child process timeout = 10 minutes: more than sufficient on dual 2.13GHz E5506 XEON, 
 16 Gbyte workstation with GTX 750 GPU 
 """
-def parasite(proc):
-    """ Read stdout from the proc and write lines to given file """
-    with open ("c:\\temp\\gmat_stdout.log",'+a') as fout:
-        for line in iter(proc.stdout.readline, b''):
-            fout.write('got line: {0}'.format(line.decode('utf-8')))
+#def parasite(proc):
+#    """ Read stdout from the proc and write lines to given file """
+#    with open ("c:\\temp\\gmat_stdout.log",'+a') as fout:
+#        for line in iter(proc.stdout.readline, b''):
+#            fout.write('got line: {0}'.format(line.decode('utf-8')))
+
+def run_gmat(gmat_arg):
+    msg[0] = "Running GMAT with File: " + str(gmat_arg)
+    msg[1] = ""
+    
+    proc = sp.Popen(['gmat', '-m', '-ns', '-x', '-r', str(gmat_arg)], stdout=sp.PIPE, stderr=sp.STDOUT)   
+    """ Run GMAT for batch file path names as gmat_args.
+    GMAT Arguments:
+    -m: Start GMAT with a minimized interface.
+    -ns: Start GMAT without the splash screen showing.
+    -x: Exit GMAT after running the specified script.
+    -r: Automatically run the specified script after loading.
+    """
+    try:
+        """ The buffer passed to Popen() defaults to io.DEFAULT_BUFFER_SIZE, usually 62526 bytes.
+        If this is exceeded, the child process hangs with write pending for the buffer to be read.
+        https://thraxil.org/users/anders/posts/2008/03/13/Subprocess-Hanging-PIPE-is-your-enemy/
+        This try block will attempt to maintain the buffer by reading it frequently, otherwise the timeout
+        value should be long enough for GMAT to complete, the TimeoutExpired exeception allows 
+        processing to continue under this assumption.
+        """
+        (outs, errors) = proc.communicate(cpto)
+        """Timeout in cpto seconds if process does not complete."""
         
+    except sp.TimeoutExpired as e:
+        """ This function is meant to be called in the multiprocess context.  Logging
+        threads are dangerous, because the thread context from the parent process is not passed
+        to the child process.  Logging must be done in the parent process.
+        """
+        msg[1] += "GMAT timed out in child process " + str(proc.pid) + "."
+        
+        proc.kill()
+        """ The child process is not killed by the system, so clean it up here."""
+        (outs, errors) = proc.communicate()
+        """ And the stdout buffer must be flushed. """
+            
+    finally:
+        return(msg)
+    
 class GMAT_Path:
     """ This class initializes its instance with the GMAT root path using the 
     'LOCALAPPDATA' environment variable.  Current version is Windows specific.
@@ -83,6 +126,10 @@ class GMAT_Path:
 
 if __name__ == "__main__":
     """ Retrieve the batch file and run GMAT for each model file listed """
+    __spec__ = None
+    """ Tweak to get Spyder IPython to execute this code. See:
+    https://stackoverflow.com/questions/45720153/python-multiprocessing-error-attributeerror-module-main-has-no-attribute
+    """
     logging.basicConfig(
             filename='./appLog.log',
             level=logging.INFO,
@@ -104,70 +151,65 @@ if __name__ == "__main__":
         
     logging.info('Batch file is %s', fname[0])
     
-    gmat_args = ()
+    gmat_arg = ()
+    gmat_args = list()
+    ncpus = mp.cpu_count()
+    nrunp = ncpus - rsrv_cpus
+    ninstances = 4
     
     try:
-        with open(fname[0]) as f:    
+        with open(fname[0]) as f:
+            """ This is the master batch file selected in QtFileDialog. """
             for filename in f:
-                gmat_args = os.path.normpath(filename)
+                """ It must be cleaned up for GMAT to recognize it. """
+                gmat_arg = os.path.normpath(filename)
                 rege = re.compile('\n')
-                gmat_args = rege.sub('', gmat_args)
+                gmat_arg = rege.sub('', gmat_arg)
 
-                logging.debug("Path to scriptfile is %s", gmat_args)
+                logging.debug("Path to scriptfile is %s", gmat_arg)
                 scriptname = os.path.basename(filename)
-                logging.info("GMAT will be called for script %s", scriptname)
-               
-                proc = sp.Popen(['gmat', '-m', '-ns', '-x', '-r', str(gmat_args)], stdout=sp.PIPE, stderr=sp.STDOUT)   
-                """ Run GMAT for each file in batch.
-                    Arguments:
-                    -m: Start GMAT with a minimized interface.
-                    -ns: Start GMAT without the splash screen showing.
-                    -x: Exit GMAT after running the specified script.
-                    -r: Automatically run the specified script after loading.
-                Note: The buffer passed to Popen() defaults to io.DEFAULT_BUFFER_SIZE, usually 62526 bytes.
-                If this is exceeded, the child process hangs with write pending for the buffer to be read.
-                https://thraxil.org/users/anders/posts/2008/03/13/Subprocess-Hanging-PIPE-is-your-enemy/
-                """
-                try:
-                    (outs, errors) = proc.communicate(cpto)
-                    """Timeout in cpto seconds if process does not complete."""
-                    
-                except sp.TimeoutExpired as e:
-                    logging.error('GMAT timed out in child process. Time allowed was %s secs, continuing', str(cpto))
-                    
-                    logging.info("Process %s being terminated.", str(proc.pid))
-                    proc.kill()
-                    """ The child process is not killed by the system. """
-                    
-                    (outs, errors) = proc.communicate()
-                    """ And the stdout buffer must be flushed. """
-                           
+                
+                gmat_args.append(gmat_arg)
+                                      
+        with mp.Pool(processes=nrunp, maxtasksperchild=20 ) as workers:
+            results_iter = workers.imap(run_gmat, gmat_args, ninstances)
+                
+        for msg in results_iter:
+            logging.info(msg[0])
+            if msg[1].len > 0:
+                logging.error(msg[1])
+            
+    except sp.CalledProcessError as e:       
+        msg[1] += "GMAT error code, " + e.returncode + " returned."
+        msg[1] += "\nError is: " + e.stderr    
+    
+    except sp.SubprocessError as e:
+        msg[1] += "Popen generic child process error, " + e.args + "."
+        
+    except ValueError as e:
+        msg[1] += "Subprocess called with incorrect arguments: " + e.args + "."
+        
+    except AttributeError as e:
+#        tb = sys.exc_info()      
+        lines = traceback.format_exc().splitlines()
+        msg[1] += "Cause: " + e.__doc__ + "Context: " + e.__cause__ + "Traceback:\n"
+        msg[1] += lines[0] + lines[-1]
+
     except OSError as e:
         logging.error("OS error: %s for filename %s", e.strerror, e.filename)
 
-    except sp.CalledProcessError as e:       
-        logging.error('GMAT error code, %s returned.\nError is: %s.', e.returncode, e.stderr)
-    
-    
-    except sp.SubprocessError as e:
-        logging.error('GMAT generic child process error %s.', e.args)
-        
-    except ValueError as e:
-        logging.error('Subprocess called with incorrect arguments: %s.', e.args)
-        
-    except AttributeError as e:
-        tb = sys.exc_info()
-        lines = traceback.format_exc().splitlines()
-        logging.error('%s, Cause: %s, Context: %s\n%s%s', e.__doc__, e.__cause__, e.__context__, lines[0], lines[-1])
-
     except Exception as e:
-        tb = sys.exc_info()
+#        tb = sys.exc_info()
         lines = traceback.format_exc().splitlines()
         logging.error('%s Cause: %s, Context: %s\n%s%s', e.__doc__,  e.__cause__, e.__context__, lines[0], lines[-1])
-
+                
     except:
         logging.error("Unknown error:\n%s", sys.exc_info())
-
+            
+    finally:
+        workers.close()
+        workers.join()
+        logging.info('******************** GMAT Batch Execution Completed ********************')
             
             
             
